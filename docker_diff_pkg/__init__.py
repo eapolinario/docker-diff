ea5153f0dfc2043eaebe151a8583646ae4e4a347
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2025-09-09 23:07:08 -0400

    feat: add initial implementation of Docker image comparison database manager with CLI support

---
+++ docker_diff_pkg/__init__.py
@@ -0,0 +1,8 @@
+"""
+Docker Image Comparison Database Manager
+Provides functions to store and query Docker image file comparison results in SQLite
+"""
+
+from .cli import DockerImageDB, main
+
+__version__ = "0.0.2"
